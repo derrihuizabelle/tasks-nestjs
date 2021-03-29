26d44fae98adffec07f96e0ac5b4304619273a9c
Izabelle Araujo <izabelleard@taginfraestrutura.com.br>
2021-03-28 22:12:49 -0300

    add: filter searching

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,8 +6,10 @@ import {
   Param,
   Post,
   Patch,
+  Query,
 } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
@@ -16,7 +18,10 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTaskWithFilters(filterDto);
+    }
     return this.tasksService.getAllTasks();
   }
 
