86f6f3a4d6d02b72d8baec21604e0de166e9f797
Izabelle Araujo <izabelleard@taginfraestrutura.com.br>
2021-04-09 21:22:51 -0300

    eu tava doida, fix do fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TaskRepository } from './tasks/task.repository';
-import { TasksController } from './tasks/tasks.controller';
-import { TasksService } from './tasks/tasks.service';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
-  controllers: [TasksController],
-  providers: [TasksService],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
 })
-export class TasksModule {}
+export class AppModule {}
