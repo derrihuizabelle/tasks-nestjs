32833d085dfe74cdf55f952c1cde9a40a452702e
Izabelle Araujo <izabelleard@taginfraestrutura.com.br>
2021-05-07 22:27:07 -0300

    feat: add env config for postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig.getTypeOrmConfig()),
+    TasksModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,46 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'root',
-  database: 'taskmanagement',
-  autoLoadEntities: true,
-  synchronize: true,
-};
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+class TypeOrmConfig {
+  constructor(private env: { [k: string]: string | undefined }) {}
+
+  private getValue(key: string, throwOnMissing = true): string {
+    const value = this.env[key];
+    if (!value && throwOnMissing) {
+      throw new Error(`config error - missing env.${key}`);
+    }
+
+    return value;
+  }
+
+  public ensureValues(keys: string[]) {
+    keys.forEach((k) => this.getValue(k, true));
+    return this;
+  }
+
+  public getTypeOrmConfig(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+
+      host: this.getValue('POSTGRES_HOST'),
+      port: parseInt(this.getValue('POSTGRES_PORT')),
+      username: this.getValue('POSTGRES_USER'),
+      password: this.getValue('POSTGRES_PASSWORD'),
+      database: this.getValue('POSTGRES_DATABASE'),
+    };
+  }
+}
+
+const typeOrmConfig = new TypeOrmConfig(process.env).ensureValues([
+  'POSTGRES_HOST',
+  'POSTGRES_PORT',
+  'POSTGRES_USER',
+  'POSTGRES_PASSWORD',
+  'POSTGRES_DATABASE',
+]);
+
+export { typeOrmConfig };
