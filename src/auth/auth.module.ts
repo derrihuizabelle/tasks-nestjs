a6c5881f357146a050d7582becfecaa8169fbecb
Izabelle Araujo <izabelleard@taginfraestrutura.com.br>
2021-05-11 16:26:26 -0300

    feat: jwt authorization

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
-
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.register({
+      secret: 'senhadanasa',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,34 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private _userRepository: UserRepository,
+    private _jwtService: JwtService,
   ) {}
 
   signUp(authCredentialsDto: AuthCredentialsDto) {
     return this._userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto) {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const username = await this._userRepository.validadeUserPassword(
       authCredentialsDto,
     );
     if (!username) {
       throw new UnauthorizedException('Invalid Credentials');
     }
+
+    const payload: JwtPayload = { username };
+    const accessToken = await this._jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
