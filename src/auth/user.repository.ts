639a46cdd9870f5cc2508bdd14ef01930cf532c1
Izabelle Araujo <izabelleard@taginfraestrutura.com.br>
2021-05-10 22:22:51 -0300

    feat: password hash with salt, not allow  same usernames

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,15 +1,34 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-    const { username, password } = authCredentialsDto;
+    try {
+      const { username, password } = authCredentialsDto;
 
-    const user = new User();
-    user.username = username;
-    user.password = password;
-    await user.save();
+      const user = new User();
+      user.username = username;
+      user.salt = await bcrypt.genSalt();
+      user.password = await this.hashPassword(password, user.salt);
+
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
