6fadb66df459c9e63349e86dc045a56f238badfa
Izabelle Araujo <izabelleard@taginfraestrutura.com.br>
2021-07-18 20:44:06 -0300

    going live

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,7 +3,13 @@ import * as config from 'config';
 
 const dbConfig = config.get('db');
 
+const isProduction = config.get('stage') === 'prod';
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
+  ssl: isProduction,
+  extra: {
+    ssl: isProduction ? { rejectUnauthorized: false } : null,
+  },
   type: dbConfig.type,
   host: process.env.RDS_HOSTNAME || dbConfig.host,
   port: process.env.RDS_PORT || dbConfig.port,
